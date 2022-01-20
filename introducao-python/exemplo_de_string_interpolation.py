81c02693b6eb6fd328f2067418c1864713bc15bb
Marcos <marcosviniciohd@hotmail.com>
2022-01-20 09:28:34 -0300

    Aula sobre interpolação com python

---
+++ introducao-python/exemplo_de_string_interpolation.py
@@ -0,0 +1,27 @@
+print(" ***************************")
+print("Bem vindo ao jogo de adivinhação")
+print("*****************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite seu número: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+    rodada = rodada + 1
