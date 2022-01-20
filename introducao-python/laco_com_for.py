853520eceb7614db7cc5001ed75500f0737fc575
Marcos <marcosviniciohd@hotmail.com>
2022-01-20 11:46:20 -0300

    for, break, continue

--- introducao-python/laco_com_for.py
+++ introducao-python/laco_com_for.py
@@ -6,11 +6,15 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite seu número: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -24,7 +28,6 @@ while(rodada <= total_de_tentativas):
         elif(menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
 
-    rodada = rodada + 1
 
 print("*****************************************")
 print("FIM JOGO")
